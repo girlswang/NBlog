76a9f377d714f61f8d1c91df0b7fe845d88ed4c5
tangxiaolang <1448530126@qq.com>
2016-03-20 16:02:46 +0800

    实现了留言

--- models/post.js
+++ models/post.js
@@ -27,7 +27,8 @@ Post.prototype.save = function(callback){
 		name: this.name,
 		time: time,
 		title: this.title,
-		post: this.post
+		post: this.post,
+		comments: []
 	};
 
 	//打开数据库
@@ -120,7 +121,12 @@ Post.getOne = function(name, day, title, callback){
 				}
 
 				//解析markdown 为html
-				doc.post = markdown.toHTML(doc.post);
+				if(doc){
+					doc.post = markdown.toHTML(doc.post);
+					doc.comments.forEach(function(comment){
+						comment.content = markdown.toHTML(comment.content);
+					});
+				}
 				callback(null, doc); //返回查询的一篇文章
 			});
 		});
@@ -157,3 +163,64 @@ Post.edit = function(name, day, title, callback){
 	});
 };
 
+//更新一篇文章以及相关信息
+Post.update = function(name, day, title, post, callback){
+	//打开数据库
+	mongodb.open(function(err, db){
+		if(err){
+			return callback(err);
+		}
+		//读取 posts 集合
+		db.collection('posts', function(err, collection){
+			if(err){
+				mongodb.close();
+				return callback(err);
+			}
+			//更新文章内容
+			collection.update({
+				"name": name,
+				"time.day": day,
+				"title": title
+			},{
+				$set: {post: post}
+			}, function(err){
+				mongodb.close();
+				if(err){
+					return callback(err);
+				}
+				callback(null);
+			});
+		});
+	});
+};
+
+//删除一篇文章
+Post.remove = function(name, day, title, callback){
+	//打开数据库
+	mongodb.open(function(err, db){
+		if(err){
+			return callback(err);
+		}
+		//读取 posts 集合
+		db.collection('posts', function(err, collection){
+			if(err){
+				mongodb.close();
+				return callback(err);
+			}
+			//根据用户名、日期和标题查找并删除一篇文章
+			collection.remove({
+				"name": name,
+				"time.day": day,
+				"title": title
+			},{
+				w:1
+			},function(err){
+				mongodb.close();
+				if(err){
+					return callback(err);
+				}
+				callback(null);
+			});
+		});
+	});
+};
